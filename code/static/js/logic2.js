a75ef354b26a7637f3b229e508a950b05b1af7a9
jflypilon <131604120+jflypilon@users.noreply.github.com>
2023-08-24 20:11:08 -0500

    Update logic2.js
    Using Chart.js

--- code/static/js/logic2.js
+++ code/static/js/logic2.js
@@ -1,44 +1,21 @@
-//
-
-
-//line plot for morbidities over time by year
-const xValues = [yearStart];
-
-new Chart("myChart", {
-    type: "line",
-    data: {
-        labels: xValues,
-        datasets: [{
-            data: [topic[0]],
-            borderColor: "red",
-            fill: false
-        }, {
-            data: [topic[1]],
-            borderColor: "green",
-            fill: false
-        }, {
-            data: [topic[2]],
-            borderColor: "blue",
-            fill: false
-        }, {
-            data: [topic[3]],
-            borderColor: "orange",
-            fill: false
-        }, {
-            data: [topic[4]],
-            borderColor: "pink",
-            fill: false
-        }, {
-            data: [topic[5]],
-            borderColor: "purple",
-            fill: false
-        }, {
-            data: [topic[6]],
-            borderColor: "yellow",
-            fill: false
-        }]
-    },
-    options: {
-        legend: { display: true }
-    }
-});
+//Add a link to the providing CDN (Content Delivery Network)
+< !DOCTYPE;
+html > ;
+React.createElement("html", null,
+    React.createElement("script", { src: "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js" }),
+    React.createElement("body", null,
+        "//Add a canvas to where you want to draw the chart:",
+        React.createElement("canvas", { id: "myChart", style: "width:100%;max-width:700px" }),
+        React.createElement("script", null,
+            "//Line Chart Syntax const xValues = [topic]; const yValues = [dataValue]; new Chart(\"myChart\", ",
+            type,
+            ": \"line\", data: ",
+            labels,
+            ": xValues, yValues datasets: [",
+            data,
+            ": [topic], borderColor: \"red\", fill: false backgroundColor: barColors }] }, options: ",
+            legend,
+            ": ",
+            display,
+            ": false} } });"),
+        ";"));
