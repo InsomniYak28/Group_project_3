926028e9dc7177690a17f0f591e5ba142215b9da
InsomniYak28 <131570415+InsomniYak28@users.noreply.github.com>
2023-08-25 22:38:53 -0500

    update logic 1 and 2

--- code/static/js/logic.js
+++ code/static/js/logic.js
@@ -1,73 +1,128 @@
 //api link
 const url = "http://127.0.0.1:5000/data";
 
-//function updateCharts
-function updateCharts(sample) {
-    console.log(sample);
+async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-    //iterate through sample and metadata, store as sampleNames and metaData respectively
-    d3.json(url).then(function (data) {
-        let dataPoints = data;
+    let statesArray = [];
+    let topicArray = [];
+    let descArray = [];
+    let yearArray = [];
 
-        let dataPoint = dataPoints.filter(x => x.id == data);
-        console.log(dataPoint);
-
-        let result = dataPoint[0];
-        console.log(result);
-
-        //define variables
-        let locDesc = result.locationdesc.slice(0,52).reverse();
-        let topic = result.topic.slice(0,7).reverse();
-        let topicDesc = result.topicdesc.slice(0,13).reverse();
-        //let dataValue = result.datavalue;
-        //let category = result.category.slice(0,3).reverse();
-        //let catDesc = result.categorydescslice(0,8).reverse();
-        //let catAbbr = result.categoryabbr.slice(0,8).reverse();
-        //let lat = result.lat;
-        //let lon = result.lon;
-        //let year = result.year.slice(0,11).reverse();
-        //let locAbbr = result.locationabbr.slice(0,52).reverse();
-
-        //pie chart by state, top morbidities
-        let trace1 = [{
-          values: topic,
-          labels: topic,
-          type: "pie",
-          name: locDesc,
-          hoverinfo: "label+percent",
-          textposition: "outside"
-        }];
-        let layout = {
-            height: 400,
-            width: 500,
-            title: "Mortality % in ${locDesc}"
-        };
-
-        Plotly.newPlot("pie", trace1, layout);
+    let result = data[0];
+    result.forEach(result => {
+        let locDesc = result.locationdesc;
+        statesArray.push(locDesc.str.slice(0,).reverse());
+        let topic = result.topic;
+        topicArray.push(topic).str.slice(0,).reverse();
+        let topicDesc = result.topicdesc;
+        descArray.push(topicDesc.slice(0,).reverse());
+        let year = result.year;
+        yearArray.push(year.slice(0,).reverse());
     });
 }
+
+async function ChartIt() {
+    await getData();
+    let element = document.getElementById('pie');
+    let pieChart = new ChartIt(element, {
+        type: 'pie',
+        data: {
+            labels: topicArray,
+            datasets: [{
+                backgroundColor: barColors,
+                data: topic,
+            }]
+        },
+        options: {
+            title: {
+                display: "true",
+                text: "Mortality % in ${descArray}"
+            }
+        }
+    });
+}
+
+ChartIt();
+
+// //function updateCharts
+// function updateCharts(sample) {
+//     console.log(sample);
+
+//     //iterate through sample and metadata, define vars
+//     d3.json(url).then(function (data) {
+//         let dataPoints = data;
+//         let dataPoint = dataPoints.filter(x => x.id == data);
+//         console.log(dataPoint);
+
+//         let result = dataPoint[0];
+//         console.log(result);
+
+//         //create arrays of vars
+//         let statesArray = [];
+//         let topicArray = [];
+//         let descArray = [];
+
+//         //define vars, push to arrays
+//         let locDesc = result.locationdesc;
+//         statesArray.push(locDesc.str.slice(0,).reverse());
+//         let topic = result.topic;
+//         topicArray.push(topic).str.slice(0,).reverse();
+//         let topicDesc = result.topicdesc;
+//         descArray.push(topicDesc.slice(0,).reverse());
+
+//         //let dataValue = result.datavalue;
+//         //let category = result.category.slice(0,3).reverse();
+//         //let catDesc = result.categorydescslice(0,8).reverse();
+//         //let catAbbr = result.categoryabbr.slice(0,8).reverse();
+//         //let lat = result.lat;
+//         //let lon = result.lon;
+//         //let year = result.year.slice(0,11).reverse();
+//         //let locAbbr = result.locationabbr.slice(0,52).reverse();
+
+//         //pie chart by state, top morbidities
+//         //.map(x => `${x}`)
+//         let trace1 = [{
+//           values: topic,
+//           labels: topicArray,
+//           type: "pie",
+//           name: locDesc.map(x => `${x}`),
+//           hoverinfo: "label+percent",
+//           textposition: "outside"
+//         }];
+//         let layout = {
+//             height: 400,
+//             width: 500,
+//             title: "Mortality % in `${locDesc}`"
+//         };
+
+//         Plotly.newPlot("pie", trace1, layout);
+//     });
+// }
 
 //update and log
-function updateMeta(params) {
-    console.log(params);
-}
-//initialize data on drop-down change
-function init() {
-    let dropDown = d3.select("#selDataset");
+// function updateMeta(params) {
+//     console.log(params);
+// }
+// //initialize data on drop-down change
+// function init() {
+//     let dropDown = d3.select("#selDataset");
 
-    d3.json(url).then(function (data) {
+//     d3.json(url).then(function (data) {
 
-        let states = data.locDesc;
-        console.log(states);
-        for (let i = 0; i < states.length; i++) {
-            dropDown.append("option").text(states[i]).property("value", states[i]);
-        }
-        updateCharts(states[0]);
-    });
-}
-//update plots
-function optionChanged(params) {
-    updateCharts(params);
-    updateMeta(params);
-};
-init();
+//         let states = data.statesArray;
+//         console.log(states);
+//         for (let i = 0; i < states.length; i++) {
+//             dropDown.append("option").text(states[i]).property("value", states[i]);
+//         }
+//         updateCharts(states[0]);
+//     });
+// }
+// //update plots
+// function optionChanged(params) {
+//     updateCharts(params);
+//     updateMeta(params);
+// };
+// init();
--- code/static/js/logic2.js
+++ code/static/js/logic2.js
@@ -1,21 +1,113 @@
-//Add a link to the providing CDN (Content Delivery Network)
-< !DOCTYPE;
-html > ;
-React.createElement("html", null,
-    React.createElement("script", { src: "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js" }),
-    React.createElement("body", null,
-        "//Add a canvas to where you want to draw the chart:",
-        React.createElement("canvas", { id: "myChart", style: "width:100%;max-width:700px" }),
-        React.createElement("script", null,
-            "//Line Chart Syntax const xValues = [topic]; const yValues = [dataValue]; new Chart(\"myChart\", ",
-            type,
-            ": \"line\", data: ",
-            labels,
-            ": xValues, yValues datasets: [",
-            data,
-            ": [topic], borderColor: \"red\", fill: false backgroundColor: barColors }] }, options: ",
-            legend,
-            ": ",
-            display,
-            ": false} } });"),
-        ";"));
+
+trace1 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[0],
+    line: {
+        color: 'Red',
+        width: 3
+    }
+};
+
+trace2 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[1],
+    line: {
+        color: 'Blue',
+        width: 1
+    }
+};
+trace3 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[2],
+    line: {
+        color: 'Green',
+        width: 1
+    }
+};
+trace4 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[3],
+    line: {
+        color: 'Purple',
+        width: 1
+    }
+};
+trace5 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[4],
+    line: {
+        color: 'Orange',
+        width: 1
+    }
+};
+trace6 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[5],
+    line: {
+        color: 'Yellow',
+        width: 1
+    }
+};
+trace7 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[6],
+    line: {
+        color: 'Pink',
+        width: 1
+    }
+};
+trace8 = {
+    type: 'scatter',
+    x: yearArray,
+    y: topic,
+    mode: 'lines',
+    name: topicArray[7],
+    line: {
+        color: 'Black',
+        width: 1
+    }
+};
+
+let layout = {
+    title: "Mortality Comparison by Year",
+    width: 500,
+    height: 500,
+    legend: {
+        y: 0.5,
+        traceorder: 'reversed',
+        font: {
+            size: 16
+        }
+    },
+    xaxis: {
+        title: 'Year'
+    },
+    yaxis: {
+        title: 'number of single, reported mortalities'
+    }
+};
+
+var data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7];
+
+Plotly.newPlot('line', data, layout);
