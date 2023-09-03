77cb7a75ccd7c173eefeea87375ac684eb320a6e
InsomniYak28 <131570415+InsomniYak28@users.noreply.github.com>
2023-09-03 18:49:51 -0500

    completed logic 1 push

--- code/static/js/logic.js
+++ code/static/js/logic.js
@@ -2,83 +2,87 @@
 const url = "http://127.0.0.1:5000/data";
 
 
-//function updateCharts
+//function updateCharts, creates pie chart based on arrays from function init()
 function updateCharts(sample) {
-    console.log(sample);
+    console.log("sample", sample);
 
-    //iterate through sample and metadata, define vars
     d3.json(url).then(function (data) {
-        console.log(data);
+        let result = data.data;
 
-        let result = data[0];
-        console.log(result);
+        let stateTopicData = result.filter(item => item["locationdesc"] == sample);
+        console.log("stateTopicData", stateTopicData);
 
-        //create arrays of vars
-        let statesArray = [];
-        let topicArray = [];
-        let descArray = [];
-        let yearArray = [];
+        let myData = {};
+        console.log("myData", myData);
 
-        //define vars, push to arrays
-        let locDesc = result.locationdesc;
-        statesArray.push(locDesc.str.slice(0,).reverse());
-        let topic = result.topic;
-        topicArray.push(topic).str.slice(0,).reverse();
-        let topicDesc = result.topicdesc;
-        descArray.push(topicDesc.slice(0,).reverse());
-        let year = result.year;
-        yearArray.push(year.slice(0,).reverse());
+        stateTopicData.forEach(item => {
+            let value = item["topic"];
+            if (!myData[value]) {
+                myData[value] = 1;
+            } else {
+                myData[value]++;
+            }
+        });
 
-        //let dataValue = result.datavalue;
-        //let category = result.category;
-        //let catDesc = result.categorydesc;
-        //let catAbbr = result.categoryabbr;
-        //let lat = result.lat;
-        //let lon = result.lon;
-        //let locAbbr = result.locationabbr;
+        let labels = Object.keys(myData);
+        console.log("labels", labels);
+        let values = Object.values(myData);
+        console.log("values", values);
 
-        //pie chart by state, top morbidities
-        //.map(x => `${x}`)
-        let trace1 = [{
-            values: topic,
-            labels: topicArray,
-            type: "pie",
-            name: locDesc,
-            hoverinfo: "label+percent",
-            textposition: "outside"
+
+        // Create Pie chart using Plotly
+        let pieData = [{
+            labels: labels,
+            values: values,
+            type: "pie"
         }];
+
         let layout = {
-            height: 400,
-            width: 500,
-            title: "Mortality % in Chosen State"
+            title: {
+                text: "Mortality %"
+            },
         };
 
-        Plotly.newPlot("pie", trace1, layout);
+        Plotly.newPlot("pie", pieData, layout);
     });
 }
 
 
 //update and log
-function updateMeta(params) {
-    console.log(params);
+function updateMeta(sample) {
+    console.log(sample);
 }
+
+
 //initialize data on drop-down change
 function init() {
     let dropDown = d3.select("#selDataset");
 
     d3.json(url).then(function (data) {
-        // let states = data.statesArray;
-        // console.log(states);
-        // for (let i = 0; i < states.length; i++) {
-        //     dropDown.append("option").text(states[i]).property("value", states[i]);
-        // }
-        // updateCharts(states[0]);
-        console.log(data);
+        let result = data.data;
+
+        //remove duplicates so only unique locations are passed into statesArray
+        let statesArray = result.reduce(function (prev, curr) {
+            if (prev.states.indexOf(curr.locationdesc) === -1) {
+                prev.states.push(curr.locationdesc);
+            }
+            return prev;
+        }, { states: [] });
+
+        //dropdown event
+        for (let i = 0; i < statesArray.states.length; i++) {
+            dropDown.append("option").text(statesArray.states[i]).property("value", statesArray.states[i]);
+        }
+
+        updateCharts(data);
     });
 }
+
 //update plots
-function optionChanged(params) {
-    updateCharts(params);
-    updateMeta(params);
+function optionChanged(sample) {
+
+    updateCharts(sample);
+    updateMeta(sample);
 };
+
 init();
